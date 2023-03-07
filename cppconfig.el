5ff9124732c0fded7d1632e28eb3febbcac84ead
ivan-egorov@msk-ws-35 <ivan.egorov@ntprog.com>
2023-03-07 09:37:58 +0300

    edit files

---
+++ cppconfig.el
@@ -0,0 +1,87 @@
+;; ensure that we use only rtags checking
+;; https://github.com/Andersbakken/rtags#optional-1
+(defun setup-flycheck-rtags ()
+  (interactive)
+  (flycheck-select-checker 'rtags)
+  ;; RTags creates more accurate overlays.
+  (setq-local flycheck-highlighting-mode nil)
+  (setq-local flycheck-check-syntax-automatically nil))
+
+;; only run this if rtags is installed
+(when (require 'rtags nil :noerror)
+  ;; make sure you have company-mode installed
+  (require 'company)
+  (define-key c-mode-base-map (kbd "M-.")
+    (function rtags-find-symbol-at-point))
+  (define-key c-mode-base-map (kbd "M-,")
+    (function rtags-find-references-at-point))
+  ;; disable prelude's use of C-c r, as this is the rtags keyboard prefix
+;;  (define-key prelude-mode-map (kbd "C-c r") nil)
+  ;; install standard rtags keybindings. Do M-. on the symbol below to
+  ;; jump to definition and see the keybindings.
+  (rtags-enable-standard-keybindings)
+  ;; comment this out if you don't have or don't use helm
+  (setq rtags-use-helm t)
+  ;; company completion setup
+  (setq rtags-autostart-diagnostics t)
+  (rtags-diagnostics)
+  (setq rtags-completions-enabled t)
+  (push 'company-rtags company-backends)
+  (global-company-mode)
+  (define-key c-mode-base-map (kbd "<C-tab>") (function company-complete))
+  ;; use rtags flycheck mode -- clang warnings shown inline
+  (require 'flycheck-rtags)
+  ;; c-mode-common-hook is also called by c++-mode
+  (add-hook 'c-mode-common-hook #'setup-flycheck-rtags))
+;;(require 'rtags)
+;;(cmake-ide-setup)
+;;(add-hook 'c-mode-hook 'rtags-start-process-unless-running)
+;;(add-hook 'c++-mode-hook 'rtags-start-process-unless-running)
+
+(use-package rtags
+  :ensure t
+  :hook (c++-mode . rtags-start-process-unless-running)
+  :config (setq rtags-completions-enabled t
+		rtags-path "/home/ivan-egorov/.emacs.d/rtags/src/rtags.el"
+		rtags-rc-binary-name "/home/ivan-egorov/.emacs.d/rtags/bin/rc"
+		rtags-use-helm t
+		rtags-rdm-binary-name "/home/ivan-egorov/.emacs.d/rtags/bin/rdm")
+  :bind (("C-c E" . rtags-find-symbol)
+  	 ("C-c e" . rtags-find-symbol-at-point)
+  	 ("C-c O" . rtags-find-references)
+  	 ("C-c o" . rtags-find-references-at-point)
+  	 ("C-c s" . rtags-find-file)
+  	 ("C-c v" . rtags-find-virtuals-at-point)
+  	 ("C-c F" . rtags-fixit)
+  	 ("C-c f" . rtags-location-stack-forward)
+  	 ("C-c b" . rtags-location-stack-back)
+  	 ("C-c n" . rtags-next-match)
+  	 ("C-c p" . rtags-previous-match)
+  	 ("C-c P" . rtags-preprocess-file)
+  	 ("C-c R" . rtags-rename-symbol)
+  	 ("C-c x" . rtags-show-rtags-buffer)
+  	 ("C-c T" . rtags-print-symbol-info)
+  	 ("C-c t" . rtags-symbol-type)
+  	 ("C-c I" . rtags-include-file)
+  	 ("C-c i" . rtags-get-include-file-for-symbol)))
+
+(setq rtags-display-result-backend 'helm)
+
+
+;;======> улучшает работу Rtags 
+;;(load-user-file "helmconfig.el")
+
+;; ======> пакет для индексирования C/C++ кода и хранения постоянной базы данных ссылок, деклараций и тп.
+;;(require 'rtags)
+;;(require 'company-rtags)
+
+;;(setq rtags-completions-enabled t)
+;;(eval-after-load 'company
+;;  '(add-to-list
+;;    'company-backends 'company-rtags))
+;;(setq rtags-autostart-diagnostics t)
+;;(rtags-enable-standard-keybindings)
+
+;;(require 'rtags-helm)
+;;(setq rtags-use-helm t)
+
