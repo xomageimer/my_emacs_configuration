6aacf910412ea92c4e37876832830f65d693310d
ivan-egorov@msk-ws-35 <ivan.egorov@ntprog.com>
2023-03-12 17:11:22 +0300

    edit cmake conf

--- cmakeconfig.el
+++ cmakeconfig.el
@@ -2,13 +2,26 @@
   :config
   (projectile-mode +1))
 
-(use-package cmake-ide
-  :config
-  (cmake-ide-setup)
-  (setq cmake-ide-project-dir (projectile-project-root))
-  (setq cmake-ide-build-dir (or
-                             (expand-file-name "build/" cmake-ide-project-dir)
-                             (expand-file-name "cmake-build-debug/" cmake-ide-project-dir)
-                             (expand-file-name "cmake-build-release/" cmake-ide-project-dir)))
-)
+(defun compile-project (target)
+  (interactive "MEnter target name: ")
+  (let ((build-dir (concat (projectile-project-root) "build/"))
+        (compile-command (concat "cd " (projectile-project-root) " && cmake --build build --target " target)))
+    (compile compile-command)))
 
+(defun build-and-run-project (target)
+  (interactive "MEnter target name: ")
+  (let ((build-dir (concat (projectile-project-root) "build/"))
+        (compile-command (concat "cd " (projectile-project-root) " && cmake --build build --target " target " && " (projectile-project-root) "build/" target)))
+    (compile compile-command)
+    (gud-run (concat "gdb -i=mi " (concat (projectile-project-root) "build/" target)))))
+
+(defun build-and-debug-project (target)
+  (interactive "MEnter target name: ")
+  (let ((build-dir (concat (projectile-project-root) "build/"))
+        (compile-command (concat "cd " (projectile-project-root) " && cmake --build build --target " target)))
+    (compile compile-command)
+    (gdb (concat "gdb -i=mi " (concat (projectile-project-root) "build/" target)))))
+
+(global-set-key (kbd "<f5>") 'build-and-run-project)
+(global-set-key (kbd "<f6>") 'build-and-debug-project)
+(global-set-key (kbd "<f7>") 'compile-project)
