86272c4251446fca2e62cfa7d82fccc36f593953
xomageimer <xomageimer@Gmail.com>
2023-07-16 01:31:46 +0300

    add for csharp

--- init.el
+++ init.el
@@ -325,7 +325,7 @@
  '(ispell-dictionary nil)
  '(menu-bar-mode nil)
  '(package-selected-packages
-   '(tree-sitter-langs ccls multiple-cursors cff modern-cpp-font-lock call-graph exec-path-from-shell yasnippet srefactor-lisp srefactor ue embrace nerd-icons-dired nerd-icons deadgrep term-projectile projectile-codesearch consult-projectile helm projectile-ripgrep find-file-rg flycheck-clang-tidy fringe-helper realgud-lldb realgud quelpa google-translate pdf-tools solarized-themes lsp-mode lsp-focus flycheck-plantuml markdown-mode cmake-project clang-format+ google-c-style cl-lib cpputils-cmake clang-format edebug-x dap-mode fzf ag unicode-fonts default-font-presets counsel-projectile rg company-lsp projectile cmake-mode yasnippet-snippets realgud-jdb neotree which-key rtags-xref ivy-rtags ac-rtags magit smartparens company solarized-theme vertico consult use-package compat))
+   '(json-mode ejson-mode csharp-mode project tree-sitter-langs ccls multiple-cursors cff modern-cpp-font-lock call-graph exec-path-from-shell yasnippet srefactor-lisp srefactor ue embrace nerd-icons-dired nerd-icons deadgrep term-projectile projectile-codesearch consult-projectile helm projectile-ripgrep find-file-rg flycheck-clang-tidy fringe-helper realgud-lldb realgud quelpa google-translate pdf-tools solarized-themes lsp-mode lsp-focus flycheck-plantuml markdown-mode cmake-project clang-format+ google-c-style cl-lib cpputils-cmake clang-format edebug-x dap-mode fzf ag unicode-fonts default-font-presets counsel-projectile rg company-lsp projectile cmake-mode yasnippet-snippets realgud-jdb neotree which-key rtags-xref ivy-rtags ac-rtags magit smartparens company solarized-theme vertico consult use-package compat))
  '(recentf-mode t)
  '(safe-local-variable-values
    '((flycheck-mode . t)
@@ -465,7 +465,8 @@
 (use-package project
   :ensure t)
 
-(use-package deadgrep)
+(use-package deadgrep
+  :ensure t)
 
 (defun my/show-projectile-root ()
   "Display the current Projectile project root in the minibuffer."
@@ -499,3 +500,9 @@
 (require 'server)
 (when (not (server-running-p))
   (server-start))
+
+;; для C#:
+(use-package csharp-mode
+  :ensure t
+  :config
+  (add-hook 'csharp-mode-hook 'lsp-deferred))
